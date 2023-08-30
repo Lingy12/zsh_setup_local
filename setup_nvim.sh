e4a0c0983e0bd659d481dcab962278ee46fbab12
Lingy12 <russellgy1229@gmail.com>
2023-08-30 09:12:14 +0800

    support setting up nvim

---
+++ setup_nvim.sh
@@ -0,0 +1,17 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+
+mv nvim.appimage $HOME/bin/nvim
+
+mkdir .config/nvim
+
+# download node for local
+wget https://nodejs.org/dist/v18.17.1/node-v18.17.1-linux-x64.tar.xz
+tar xvf node-v18.17.1-linux-x64.tar.xz
+mv node-v18.17.1-linux-x64 bin/node
+echo "export PATH=$HOME/bin/node/bin:$PATH" >> ~/.zshrc
+
+git clone https://gitlab.com/Lin_GY/my_nvim.git
+cp -r my_nvim/* .config/nvim/
+
+rm -rf my_nvim node*
