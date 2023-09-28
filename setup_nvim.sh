8048717d6c4f5f1b600303bcf18e7de2e9b1d47c
lgy_workstation <you@example.com>
2023-09-28 14:39:50 +0800

    update script

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -8,8 +8,8 @@ mkdir $HOME/.config/nvim
 # download node for local
 wget https://nodejs.org/dist/v18.17.1/node-v18.17.1-linux-x64.tar.xz
 tar xvf node-v18.17.1-linux-x64.tar.xz
-mv node-v18.17.1-linux-x64 bin/node
-echo "export PATH=$HOME/bin/node/bin:$PATH" >> ~/.zshrc
+mv node-v18.17.1-linux-x64 $HOME/bin/node
+echo "export PATH=$HOME/bin/node/bin:$HOME/bin:$PATH" >> ~/.zshrc
 
 git clone https://gitlab.com/Lin_GY/my_nvim.git
 cp -r my_nvim/* $HOME/.config/nvim/
